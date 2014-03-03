327c085099573ab1a076ea8ea1ec403f901420fb
Nikke <nileskel@gmail.com>
2014-03-03 15:09:07 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pupkee::Application.config.secret_key_base = '447440db29da0796e3b33aca7efe7dfeecbb0bde599e5cc5b4cd6859191a0f7436799d444accbde1a8b575eef833e7d16a776d4769a3e9f3d182789eba91e5d4'
